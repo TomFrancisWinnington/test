f28957205d1f2b8bc1b889b0e2d9ff545e6522f0
tom <dopoh.tf@gmail.com>
2017-08-25 13:45:47 +0100

    chapter 4

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,14 +1,85 @@
 /*GET 'home' page */
 module.exports.homelist = function(req,res){
-	res.render('locations-list',{title:'Home'});
+	res.render('locations-list',{
+		title: 'Loc8r - find a place to work with wifi',
+		pageHeader:{
+			title: 'Loc8r',
+			strapline: 'Find places to work with wifi near you!'
+		},
+		sidebar: " Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+		locations: [{
+			name: 'starcups',
+			address:'1234 highstreet',
+			rating: 3,
+			facilities: ['hotdrinks','food','premium wifi'],
+			distance: '100m'
+		},{
+			name: 'cafe hero',
+			address:'123 highstreet',
+			rating: 5,
+			facilities: ['hotdrinks','food','premium wifi'],
+			distance: '200m'
+		},{
+			name: 'Lukes lobster',
+			address: 'muswell hill',
+			rating: 4,
+			facilities:['washlet', 'tea and coffee making facilities', 'knife', 'spoon'],
+			distance: '1.5km'
+		}]
+	});
 ;}
 
 /*GET 'Location info' page */
 module.exports.locationInfo = function(req,res){
-	res.render('location-info',{title:'Location info'});
-;}
+	res.render('location-info',{
+		title: 'Starcups',
+		pageHeader:{title:'Starcups'},
+		sidebar:{
+			context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+			callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+		},
+		location: {
+			name:'Starcups',
+			address:'1234 highstreet',
+			rating: 3,
+			facilities: ['hotdrinks','food','premium wifi'],
+			coords: {lat: 51.616254, lng: -0.161411},
+			openingTimes: [{
+				days:'Monday - Friday',
+				opening: '7:00am',
+				closing: '7:00pm',
+				closed: false
+			},{
+				days:'Saturday',
+				opening: '8:00am',
+				closing: '5:00pm',
+				closed: false
+			},{
+				days: 'Sunday',
+				closed: true
+			}],
+			reviews: [{
+				author: 'barry scott',
+				rating: 5,
+				timestamp: '15 July 2017',
+				reviewText: 'what a great place, truely excellent'
+			},{
+			
+				author: 'mark overmars',
+				rating: 5,
+				timestamp: '17 July 2017',
+				reviewText: 'bit of alright this!'	
+	
+			}]
+		}
+
+	});
+};
 
 /*GET 'Add review' page */
 module.exports.addReview = function(req,res){
-	res.render('location-review-form',{title:'Add review'});
+	res.render('location-review-form',{
+		title:'Review Starcups on Loc8r',
+		pageHeader: {title: 'Review Starcups'}
+		});
 ;}
